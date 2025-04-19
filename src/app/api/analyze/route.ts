0fa623d75ed407c73213e806e35a8d847e445d45
Axel Gil <axelgil60@gmail.com>
2025-04-19 22:03:11 +0200

    Implement GET endpoint to fetch and log GitHub repository metadata and file tree

---
+++ src/app/api/analyze/route.ts
@@ -0,0 +1,51 @@
+import { fetchRepoFileTree, fetchRepoMetadata, filterFiles } from "@/lib/github";
+import { RepoMetadata } from "@/types/repo";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const url = searchParams.get("repo");
+
+  if (!url) {
+    return new Response("Missing repo URL", { status: 400 });
+  }
+
+  try {
+    const urlTest = "https://github.com/vercel/next.js";
+
+    const res = await fetchRepoMetadata(urlTest);
+    const metadata: RepoMetadata = {
+      owner: res.owner.login,
+      name: res.name,
+      description: res.description,
+      created_at: res.created_at,
+      updated_at: res.updated_at,
+      pushed_at: res.pushed_at,
+      size: res.size,
+      stargazers_count: res.stargazers_count,
+      forks_count: res.forks_count,
+      watchers_count: res.watchers_count,
+      open_issues_count: res.open_issues_count,
+      subscribers_count: res.subscribers_count,
+      pulls_url: res.pulls_url,
+      default_branch: res.default_branch,
+      trees_url: res.trees_url,
+    }
+    console.log(metadata);
+
+    const fileTree = await fetchRepoFileTree(metadata.trees_url);
+    
+    const filteredFiles = await filterFiles(fileTree);
+    console.log(filteredFiles);
+
+    // const analysis = await analyzeRepo(filteredFiles);
+
+    // return NextResponse.json(analysis, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Error fetching repo metadata" },
+      { status: 500 }
+    );
+  }
+}
